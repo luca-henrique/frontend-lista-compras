1727534426abff6e4a40ca288546ca73d65074b7
Lucas Henrique <lukas.paes18@gmail.com>
2022-08-03 17:32:34 -0300

    feature:adicionando theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {createTheme} from '@material-ui/core/styles';
+
+import {ThemeProvider} from '@material-ui/styles';
+
+import {AppBar} from '@material-ui/core';
+
+import './style.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#e91e63',
+    },
+    secondary: {
+      main: '#00b0ff',
+    },
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider theme={theme}>
+      <AppBar position='static' color='primary'>
+        <div className='header'>
+          <p className='title'>Lista de Compras</p>
+        </div>
+      </AppBar>
+      <div className='App'>
+        <h1>Example</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;

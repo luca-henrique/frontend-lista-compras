c6753c8630ca39dc81b03e44172d7b032d512247
Lucas Henrique <lukas.paes18@gmail.com>
2022-08-03 22:24:19 -0300

    feature: #40 iniciando home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,26 @@
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+} from '@material-ui/core';
+
+import './style.css';
+
 const Home = () => {
   return (
-    <div className='App'>
-      <h1>Home Page</h1>
+    <div className='page-container'>
+      <div className='new-list-container'>
+        <Card className='card'>
+          <CardActionArea className='card-action-area'>
+            <CardContent className='card-content'>
+              <div>
+                <p className='title'>Adicionar novas Listas!</p>
+              </div>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
     </div>
   );
 };

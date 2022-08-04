50e6079182c2e0249ea20fca8bb66fc95fce2792
Lucas Henrique <lukas.paes18@gmail.com>
2022-08-03 22:35:40 -0300

    feature: #41 criando lista

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,9 +3,13 @@ import {
   CardActionArea,
   CardActions,
   CardContent,
+  Divider,
 } from '@material-ui/core';
 
 import './style.css';
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faShoppingBasket, faCheck} from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
   return (
@@ -19,6 +23,33 @@ const Home = () => {
               </div>
             </CardContent>
           </CardActionArea>
+        </Card>
+      </div>
+
+      <div className='list-container'>
+        <Card className='card'>
+          <CardActionArea className='card-action-area'>
+            <CardContent className='card-content'>
+              <div>
+                <p className='title'>Mês</p>
+                <div className='list-card-body'>
+                  <div className='list-card-item'>
+                    <FontAwesomeIcon icon={faShoppingBasket} size='sm' />
+                    <p>Item(s) restante</p>
+                  </div>
+                  <div className='list-card-item'>
+                    <FontAwesomeIcon icon={faCheck} size='sm' />
+                    <p>Item(s) comprados</p>
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+          </CardActionArea>
+          <Divider />
+          <CardActions className='card-footer'>
+            <p>01/01/2022</p>
+            <p>100.00</p>
+          </CardActions>
         </Card>
       </div>
     </div>

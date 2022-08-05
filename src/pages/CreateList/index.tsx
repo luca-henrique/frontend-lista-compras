e4abd918ee23edebf281e8029f4242737c02ce43
Lucas Henrique <lukas.paes18@gmail.com>
2022-08-04 23:50:57 -0300

    feature: estilizando o form

--- src/pages/CreateList/index.tsx
+++ src/pages/CreateList/index.tsx
@@ -1,5 +1,63 @@
+import TextField from '@material-ui/core/TextField';
+import {InputAdornment, MenuItem} from '@material-ui/core';
+
+const units = ['kg', 'lt', 'un'];
+
 const CreateList = () => {
-  return <h1>Create List</h1>;
+  return (
+    <div className='page-container'>
+      Create List
+      <form className='form-container'>
+        <TextField
+          label='Lista'
+          name='list'
+          value={''}
+          onChange={() => {}}
+          required
+        />
+        <TextField
+          label='Producto'
+          name='product'
+          value={''}
+          onChange={() => {}}
+          required
+        />
+
+        <TextField
+          label='Quantidade'
+          name='quantity'
+          value={''}
+          onChange={() => {}}
+          required
+          select
+        >
+          {units.map((option) => {
+            return <MenuItem key={option}>{option}</MenuItem>;
+          })}
+        </TextField>
+
+        <TextField
+          label='Unidade'
+          name='unity'
+          value={''}
+          onChange={() => {}}
+          required
+        />
+
+        <TextField
+          label='Preço'
+          name='price'
+          value={''}
+          onChange={() => {}}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>R$</InputAdornment>
+            ),
+          }}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default CreateList;

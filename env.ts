7ed45416b5676e95a32d8a694811948267b55d77
Lucas Henrique <lukas.paes18@gmail.com>
2022-08-12 14:44:32 -0300

    feature: adicionando service

---
+++ env.ts
@@ -0,0 +1,2 @@
+const search = 'AIzaSyDvnr1R-YrX43DlqcxC0iSRvxOYkSbiJcU';
+const cx = 'a78bb5bbccbb640c4';
---
+++ src/service/index.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const services = {
+  getImages: (product) => {
+    const params = {
+      key: 'AIzaSyDvnr1R-YrX43DlqcxC0iSRvxOYkSbiJcU',
+      cx: 'a78bb5bbccbb640c4',
+      searchType: 'image',
+      lr: 'lang_pt',
+      q: product.productName,
+      num: 1,
+    };
+    return axios
+      .get('https://www.googleapis.com/customsearch/v1', {params})
+      .then((resp) => resp.data.items)
+      .catch((err) => err);
+  },
+};
